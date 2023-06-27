d30a4437861354cef06a78b5fbf9fc818282f8f6
NEMURENAIYORU <lhslhs301@gmail.com>
2023-06-27 22:40:04 +0900

    ✨ feat : Merge Layout component and main image

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,14 @@
+import Layout from './layout';
+import TypoImage from './layout/body/TypoImage';
+
+
+function MainPage() {
+  return (
+    <Layout>
+      <TypoImage></TypoImage>
+      
+    </Layout>
+  );
+}
+
+export default MainPage;
