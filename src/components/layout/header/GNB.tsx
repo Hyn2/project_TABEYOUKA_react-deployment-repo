a5b81c1c0aeb191c18f0882906b2acbfea4d3422
NEMURENAIYORU <lhslhs301@gmail.com>
2023-07-10 19:03:11 +0900

    🎨 refactor : Add mode status update props

--- src/components/layout/header/GNB.tsx
+++ src/components/layout/header/GNB.tsx
@@ -1,20 +1,36 @@
 import { Box, Button, Typography } from "@mui/material"
 import React from "react";
 
-const pages = [{key : 'Products', onClick :()=> {console.log("Products")}}, {key : 'Pricing', onClick :()=> {console.log("Pricing")}}, {key : 'Blog', onClick : ()=> {console.log("Blog")}}];
 
-const GNB = () => {
+
+interface page {
+    key : string
+    onClick : ()=> void
+}
+
+interface props {
+    pages : page[]
+    onClick: (value: string) => void;
+}
+const GNB = ({pages, onClick} : props) => {
     const [mode, setMode] = React.useState<string>(pages[0].key);
+    
+    // onClick시 props로 받은 onClick 함수를 실행하고, mode 상태를 업데이트
+    const handleClick = (page: string) => {
+        onClick(page);
+        setMode(page);
+    };
+
 
     return (
         <>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' },ml : 8 }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Box key={page.key} sx={{borderBottom :  `${mode === page.key && "2px red solid"}`}}>
                 <Button
                 {...page}
                   sx={{ my: 2, display: 'block', color : `${mode !== page.key ? "grey" : "black"}`, mx : 1}}
-                  onClick={()=> setMode(page.key)}
+                  onClick={()=> handleClick(page.key)}
                 >
                   <Typography variant='caption'>
                     {page.key}
