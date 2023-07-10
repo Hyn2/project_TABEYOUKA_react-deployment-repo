e90edb7986098ce5bc366f346fa8895384960f60
NEMURENAIYORU <lhslhs301@gmail.com>
2023-07-10 17:11:23 +0900

    ✨ feat : Changing the Routing Method (Dynamic Routing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './components/layout/header'
-import LoginPage from './pages/LoginPage'
-import MainPage from './pages/MainPage'
+import DynamicRouter from './Router'
+import Layout from './components/layout'
 
 
-const Layout = () => {
-  return (
-    <div>
-      <Header />
 
-      <Outlet />
-    </div>
-  )
-}
-localStorage.setItem('isLoggedIn', 'false');
+
 function App() : JSX.Element {
   
-
+  
   return (
     
-
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path='/main' element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Route>
-      </Routes>
+    <Layout>
+      <DynamicRouter />
+    </Layout>
+      
     
   )
 }
---
+++ src/Router.tsx
@@ -0,0 +1,53 @@
+import { CircularProgress } from "@mui/material";
+import { Box } from "@mui/system";
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+
+
+const modules = import.meta.glob("/src/pages/**/*.tsx");
+const routes = Object.keys(modules).map((route) => {
+  const path = route
+    .replace(/\/src\/pages|index|\.tsx$/g, "") // prefix削除
+  return { path, component: lazy(modules[route] as any) };
+});
+
+/**
+ * @description react-router-dom을 기반으로 컴포넌트 들의 페이지를 url에 맞게 로드 시키는 부분입니다.
+ * @page ./pages
+ */
+
+const DynamicRouter = () => {
+  return (
+    <Routes>
+      {routes.map(({ path, component: Component }) => {
+        return (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense
+                fallback={
+                  <Box
+                    sx={{
+                      display: "flex",
+                      minHeight: "100vh",
+                      flex: 1,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <CircularProgress />
+                  </Box>
+                }>
+                <Component />
+              </Suspense>
+            }
+          />
+        );
+      })}
+      
+    </Routes>
+  );
+};
+
+export default DynamicRouter;
