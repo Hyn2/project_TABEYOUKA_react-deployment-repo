044910e3b80d5a7ff32b69d6c4c735163845cabe
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-07-13 21:39:10 +0900

    feat(component): Add ImageCarousel component

---
+++ src/components/common/carousel/ImageCarousel.tsx
@@ -0,0 +1,117 @@
+import ReactSwipe from 'react-swipe';
+import { useEffect } from 'react';
+import { Box, IconButton } from '@mui/material';
+import { SwipeStyle } from '../../../types/react-swipe.interface';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import CloseIcon from '@mui/icons-material/Close';
+
+interface ImageCarouselProps {
+  photos: File[];
+  deleteHandler?: (index: number) => void;
+}
+
+const swipeStyle: SwipeStyle = {
+  container: {
+    overflow: 'hidden',
+    visibility: 'hidden',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+  },
+  wrapper: {
+    display: 'flex',
+    overflow: 'hidden',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  child: {
+    float: 'left',
+    width: '100%',
+    position: 'relative',
+    transitionProperty: 'transform',
+  },
+};
+
+const ImageCarousel = ({ photos, deleteHandler }: ImageCarouselProps) => {
+  let reactSwipeEl: ReactSwipe;
+
+  useEffect(() => {
+    reactSwipeEl.slide(photos.length ? photos.length - 1 : 0, 0);
+  }, [photos]);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        width: '550px',
+        height: '550px',
+        position: 'relative',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box display="flex" width="100%" height="100%" justifyContent="center" alignItems="center">
+        <ReactSwipe
+          className="carousel"
+          swipeOptions={{ continuous: false }}
+          ref={(el) => (reactSwipeEl = el as ReactSwipe)}
+          childCount={photos.length}
+          style={swipeStyle}
+        >
+          {photos.map((photo, i) => (
+            <Box key={i} height="100%" position="relative">
+              <img
+                src={URL.createObjectURL(photo)}
+                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+                alt="reviewPhoto"
+              />
+              {deleteHandler && (
+                <IconButton
+                  size="small"
+                  sx={{
+                    position: 'absolute',
+                    top: 4,
+                    right: 4,
+                    zIndex: 100,
+                    backdropFilter: 'contrast(70%)',
+                  }}
+                  onClick={() => deleteHandler(i)}
+                >
+                  <CloseIcon />
+                </IconButton>
+              )}
+            </Box>
+          ))}
+        </ReactSwipe>
+        <Box
+          sx={{
+            display: 'flex',
+            position: 'absolute',
+            width: '100%',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <IconButton
+            sx={{ left: 0, backdropFilter: 'contrast(50%)' }}
+            onClick={() => reactSwipeEl.prev()}
+          >
+            <NavigateBeforeIcon />
+          </IconButton>
+          <IconButton
+            sx={{ right: 0, backdropFilter: 'contrast(50%)' }}
+            onClick={() => reactSwipeEl.next()}
+          >
+            <NavigateNextIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ImageCarousel;
