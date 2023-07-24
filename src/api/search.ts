9ec15b4a32d2340ef5cba4102910c0afa4035284
NEMURENAIYORU <lhslhs301@gmail.com>
2023-07-24 09:35:39 +0900

    🔨 chore : Add conditional statements to get the number of searches

--- src/api/search.ts
+++ src/api/search.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 export default async function searchRestaurant(params : any) {
   try {
     const response = await axios.get("http://localhost:8000/api/search", {params: params});
-    console.log(response.data.original.shop);
-    return response.data.original.shop;
+    console.log(response);
+    if (params.count === 100) {
+      return response.data.original.results_available
+    }
+    return response.data.original.results;
   } catch (error) {
     console.error(error);
   }
