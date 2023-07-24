def3129e1c1219a2abd5ed050d00ae74984cf55a
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-07-25 02:55:27 +0900

    feat(editor): Add CKEditor interface

--- src/types/ckeditor.interface.ts
+++ src/types/ckeditor.interface.ts
@@ -1,4 +1,9 @@
+import BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { ComponentProps } from 'react';
 
 export interface EditorProps extends Omit<ComponentProps<typeof CKEditor>, 'editor'> {}
+
+export interface IEditor extends Omit<ComponentProps<typeof CKEditor<BalloonEditor>>, 'editor'> {
+  ref: EditorProps;
+}
