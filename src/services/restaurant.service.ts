5e94e37398bf04c26ef1052244cf78e8685f5cf7
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-07-27 05:14:25 +0900

    feat(restaurant): add redirect functionality to restaurant detail page

--- src/services/restaurant.service.ts
+++ src/services/restaurant.service.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import { RestaurantHOTPP } from '../types/restaurant.interface';
 
-export const findRestaurant = async (restuarant_id: string) => {
-  console.log(restuarant_id);
+export async function findRestaurant(restuarant_id: string): Promise<RestaurantHOTPP> {
   const response = await axios.get(`http://localhost:8000/api/restaurant/${restuarant_id}`);
-  console.log(response.data);
   return response.data;
-};
+}
